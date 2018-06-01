76c3e86d5cfa22a8a7e925ad9c99148969301f40
FaceFeel <995956062@qq.com>
2018-06-01 11:19:36 +0800

    1.fix

--- user-client/src/main/java/com/client/UserClientApplication.java
+++ user-client/src/main/java/com/client/UserClientApplication.java
@@ -3,13 +3,12 @@ package com.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class ClientApplication {
+public class UserClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ClientApplication.class, args);
+        SpringApplication.run(UserClientApplication.class, args);
     }
 }
